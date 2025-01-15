cb5735b700e7a647b0687ce3bb28915ce667d2e9
elsavj <elsa.vijendran@gmail.com>
2025-01-15 17:17:57 +0800

    update route

--- mysite/src/index.js
+++ mysite/src/index.js
@@ -4,21 +4,19 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage.js";
 import About from "./pages/About.js";
+import Error from "./pages/Error.js";
+
 
 export default function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}>
-          {/* <Route index element={<Homepage />} /> */}
-          <Route path="mysite" element={<Homepage />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Homepage />} />
-          {/* <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} /> */}
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
+        <Route path="/" element={<Homepage />} />
+        <Route path="mysite" element={<Homepage />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Homepage />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ mysite/src/pages/Error.js
@@ -0,0 +1,42 @@
+import '../assets/css/main.css';
+import React from "react";
+import { Link } from 'react-router-dom';
+import SidebarMenu from '../components/SidebarMenu.js';
+import DocumentMeta from 'react-document-meta';
+import Box from '@mui/material/Box';
+
+const elsaOs = "+----------------------------------------------------+\n|,------.,--.    ,---.    ,---.      ,-----.  ,---.  |\n||  .---'|  |   '   .-'  /  O   \    '  .-.  ''   .-' |\n||  `--, |  |   `.  `-. |  .-.  |   |  | |  |`.  `-. |\n||  `---.|  '--..-'    ||  | |  |   '  '-'  '.-'    ||\n|`------'`-----'`-----' `--' `--'    `-----' `-----' |\n+----------------------------------------------------+"
+
+function TextArt({ label, text }) {
+  return (
+    <pre
+      aria-label={label}
+      className="text-art ascii-art"
+    >{text}</pre>
+  );
+}
+
+function About() {
+  // const meta = {
+  //   title: 'ELSA',
+  //   // canonical: 'http://example.com/path/to/page',
+  //   meta: {
+  //     charset: 'utf-8'
+  //   }
+  // }
+
+  return (
+    // <DocumentMeta {...meta}>
+
+    <div className="About">
+      <body className="is-preload">
+      
+      ERROR 
+
+      </body>
+    </div>
+    // </DocumentMeta>
+  );
+}
+
+export default About;
